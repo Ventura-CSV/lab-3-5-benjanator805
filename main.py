ad318852b642406a559fe8e1ed08f64270460466
NotBen <benramirez805@gmail.com>
2024-06-27 15:39:31 -0700

    main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
+    
     ########################################
-    Code Your Program here
+    if  num1 == num2 and num3:
+        print("duplication = 3")
+    elif num1 == num2 or num2 == num3 or num3 == num1:
+        print("duplication = 2")
+    else: print("duplication = 0")
+   
+    
     ########################################
-    """
+    
     duplication = 0
-
     ########################################
     # Do not delete the return statement
     ########################################
